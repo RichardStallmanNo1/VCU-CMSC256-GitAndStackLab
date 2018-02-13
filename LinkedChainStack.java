517cf3c7b25b2491369e4067217ae3824ebfaaf1
RichardStallmanNo1 <imamizzile@420blaze.it>
2018-02-13 16:16:18 -0500

    linkchain

---
+++ LinkedChainStack.java
@@ -0,0 +1,70 @@
+import java.util.EmptyStackException;
+
+public final class LinkedChainStack<T> implements StackInterface<T>
+{
+    private Node topNode;
+
+    public LinkedChainStack() {
+        topNode = null;
+    }
+
+    public void push(T newEntry) {
+        topNode = new Node(newEntry, topNode);
+    }
+
+    public T peek() {
+        if (isEmpty())
+            throw new EmptyStackException();
+        else
+            return topNode.getData();
+    }
+
+    public T pop() {
+        T top = peek();
+        assert (topNode != null);
+        topNode = topNode.getNextNode();
+
+        return top;
+    }
+
+    public boolean isEmpty() {
+        return topNode == null;
+    }
+
+    public void clear() {
+        topNode = null;
+    }
+
+    private class Node {
+
+        private T    data;
+        private Node next;
+
+        private Node(T dataPortion) {
+            this(dataPortion, null);
+        }
+
+        private Node(T dataPortion, Node linkPortion) {
+            data = dataPortion;
+            next = linkPortion;
+        }
+
+        private T getData() {
+            return data;
+        }
+
+        private void setData(T newData) {
+            data = newData;
+        }
+
+        private Node getNextNode() {
+            return next;
+        }
+
+        private void setNextNode(Node nextNode) {
+            next = nextNode;
+        }
+    }
+
+}
+
