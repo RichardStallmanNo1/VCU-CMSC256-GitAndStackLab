bce8ada1b41d89812574e39dd8a982832d11131f
RichardStallmanNo1 <imamizzile@420blaze.it>
2018-02-13 15:38:45 -0500

    Fix stack

---
+++ ArrayBasedStack.java
@@ -0,0 +1,26 @@
+public class ArrayBasedStack implements StackInterface {
+    @Override
+    public void push(Object newEntry) {
+
+    }
+
+    @Override
+    public Object pop() {
+        return null;
+    }
+
+    @Override
+    public Object peek() {
+        return null;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+}
