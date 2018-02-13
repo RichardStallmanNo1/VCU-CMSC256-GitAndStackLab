8e1c5c599fd3f66079aea0d08e0c124b67543d80
RichardStallmanNo1 <imamizzile@420blaze.it>
2018-02-13 15:55:12 -0500

    ArrayList class

--- ArrayBasedStack.java
+++ ArrayBasedStack.java
@@ -1,26 +1,74 @@
-public class ArrayBasedStack implements StackInterface {
-    @Override
-    public void push(Object newEntry) {
+import java.util.Arrays;
+import java.util.EmptyStackException;
 
+
+
+public final class ArrayBasedStack<T> implements StackInterface<T> {
+    private T[] data;
+    private int topOfStack;
+    private boolean initialized = false;
+    private static final int INITIAL_CAPACITY = 50;
+
+    public ArrayBasedStack() {
+        this(INITIAL_CAPACITY);
     }
 
-    @Override
-    public Object pop() {
-        return null;
+    public ArrayBasedStack(int initialCapacity) {
+        if(initialCapacity <= 0) throw new IllegalArgumentException("Can't have <= 0 capacity");
+
+        data = (T[]) new Object[initialCapacity];
+        topOfStack = -1;
+        initialized = true;
     }
 
-    @Override
-    public Object peek() {
-        return null;
+    public void push(T newEntry) {
+        checkInitialization();
+        ensureCapacity();
+        data[topOfStack + 1] = newEntry;
+        topOfStack++;
     }
 
-    @Override
+    public T peek() {
+        checkInitialization();
+        if (isEmpty())
+            throw new EmptyStackException();
+        else
+            return data[topOfStack];
+    }
+
+    public T pop() {
+        checkInitialization();
+        if (isEmpty())
+            throw new EmptyStackException();
+        else {
+            T top = data[topOfStack];
+            data[topOfStack] = null;
+            topOfStack--;
+            return top;
+        }
+    }
+
     public boolean isEmpty() {
-        return false;
+        return topOfStack < 0;
     }
 
-    @Override
     public void clear() {
+        checkInitialization();
+        while (topOfStack > -1) {
+            data[topOfStack] = null;
+            topOfStack--;
+        }
+    }
 
+    private void checkInitialization() {
+        if (!initialized)
+            throw new SecurityException("ArrayStack object is not initialized properly.");
+    }
+
+    private void ensureCapacity() {
+        if (topOfStack >= data.length - 1) {
+            int newLength = 2 * data.length;
+            data = Arrays.copyOf(data, newLength);
+        }
     }
 }
